47647dd8b1e42d7dbfc6ab5b2d7b7ee4ab500b90
Zoltan Kiss <zkiss@ackmanndickenson.com>
2015-06-30 23:39:24 -0500

    small refactor

--- lib/thorough_test.rb
+++ lib/thorough_test.rb
@@ -1,27 +1,34 @@
 class ThoroughTest
   def initialize(path)
     @path = path
-    head_hash = (`cd #{path} && git show HEAD`).match(/commit ([a-z0-9]*)\n/)[1]
-    `cd #{path} && git reset --hard HEAD~1`
-    `cd #{path} && git cherry-pick -n #{head_hash}`
-    @non_spec_files = (`cd #{path} && git status`).
-      split("\n").
-      map(&:strip).
-      select { |line| line.match(/modified:/) && !line.match(/spec\.rb$/) }.
-      map { |l| l.match(/modified:   (.*rb)$/)[1] }
-    `cd #{path} && git reset HEAD`
-    @non_spec_files.each { |file| `cd #{path} && git checkout #{file}` }
-    @spec_files = (`cd #{path} && git status`).
-      split("\n").
-      map(&:strip).
-      select { |line| line.match(/modified:/) && line.match(/spec\.rb$/) }.
-      map { |l| l.match(/modified:   (.*rb)$/)[1] }
-    @has_failures = !!`cd #{path} && rspec #{@spec_files.join(' ')}`.match(/Failures:/)
-    `cd #{path} && git reset --hard`
-    `cd #{path} && git cherry-pick #{head_hash}`
+    head_hash = (`cd #{@path} && git show HEAD`).match(/commit ([a-z0-9]*)\n/)[1]
+    `cd #{@path} && git reset --hard HEAD~1`
+    `cd #{@path} && git cherry-pick -n #{head_hash}`
+    `cd #{@path} && git reset HEAD`
+    non_spec_files.each { |file| `cd #{@path} && git checkout #{file}` }
+    @has_failures = !!`cd #{@path} && rspec #{spec_files.join(' ')}`.match(/Failures:/)
+    `cd #{@path} && git reset --hard`
+    `cd #{@path} && git cherry-pick #{head_hash}`
   end
 
   def latest_commits_code_was_written_by_having_failing_test_first?
     @has_failures
   end
+
+  private
+    def non_spec_files
+      @non_spec_files ||= (`cd #{@path} && git status`).
+        split("\n").
+        map(&:strip).
+        select { |line| line.match(/modified:/) && !line.match(/spec\.rb$/) }.
+        map { |l| l.match(/modified:   (.*rb)$/)[1] }
+    end
+
+    def spec_files
+      @spec_files ||= (`cd #{@path} && git status`).
+        split("\n").
+        map(&:strip).
+        select { |line| line.match(/modified:/) && line.match(/spec\.rb$/) }.
+        map { |l| l.match(/modified:   (.*rb)$/)[1] }
+    end
 end
